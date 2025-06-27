b46bdcf47c2cf897579ae4c9ce5f40d25afcac1c
Thanh Truong Doan <thanh-truong.doan@mohawkcollege.ca>
2025-06-27 12:39:14 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+// npm install axios @react-navigation/native @react-navigation/native-stack react-native-screens react-native-safe-area-context
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './LoginScreen';
+import RegisterScreen from './RegisterScreen';
+import AddPassword from './AddPassword'; 
+import ShowAllPasswords from './ShowAllPasswords';
+import EditPassword from './EditPassword';
+
+const Stack = createNativeStackNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={LoginScreen}/>
+        <Stack.Screen name='Register' component={RegisterScreen}/>
+        <Stack.Screen name='AddPassword' component={AddPassword}/>
+        <Stack.Screen name='ShowAllPasswords' component={ShowAllPasswords}/>
+        <Stack.Screen name='EditPassword' component={EditPassword}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+  
+
+
