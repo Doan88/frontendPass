5604571c8b97bf0d712179ea628d8801c7b28577
Thanh Truong Doan <thanh-truong.doan@mohawkcollege.ca>
2025-07-04 14:18:45 -0400

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
-const { withExpoWebpack } = require('@expo/webpack-config');
+const webpack = require('webpack');
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
-  const config = await withExpoWebpack(env, argv);
+  const config = await createExpoWebpackConfigAsync(env, argv);
 
   config.resolve.fallback = {
     ...config.resolve.fallback,
     stream: require.resolve('stream-browserify'),
+    process: require.resolve('process/browser'),
+    crypto: require.resolve('crypto-browserify'),
   };
+
+  config.plugins = [
+    ...(config.plugins || []),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+  ];
 
   return config;
 };
